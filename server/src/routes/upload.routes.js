0ba3e8a95a58a0e0bbdaf30706cae187c998a5e1
Norris Guo <zihanguo@Zihans-MacBook-Pro.local>
2026-02-18 06:22:06 -0800

    fix upload folder creation

--- server/src/routes/upload.routes.js
+++ server/src/routes/upload.routes.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
 
 function createUploadRouter() {
   const router = express.Router();
 
+  const uploadDir = path.join(__dirname, "../../uploads");
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+  }
+
   const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, path.join(__dirname, "../../uploads"));
+      cb(null, uploadDir); 
     },
 
     filename: function (req, file, cb) {
